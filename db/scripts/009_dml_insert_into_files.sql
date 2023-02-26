15e90cd3ee6226a41f50848e40c18ac2609dddcd
MrRocketR <kuptsovns@gmail.com>
2023-02-26 22:33:21 +0200

    Исправил скрипты, добавил новые логотипы

--- db/scripts/009_dml_insert_into_files.sql
+++ db/scripts/009_dml_insert_into_files.sql
@@ -1,5 +1,8 @@
-insert into files (id, name, path) values (1, 'logo', 'img/movies/logo.png');
+insert into files (id, name, path) values (1, 'logo', 'img/logos/logo.png');
 insert into files (id, name, path) values (2, 'comedy1', 'img/movies/comedy1.png');
 insert into files (id, name, path) values (3, 'action1', 'img/movies/action1.png');
 insert into files (id, name, path) values (4, 'drama1', 'img/movies/drama1.png');
 insert into files (id, name, path) values (5, 'horror1', 'img/movies/horror1.png');
+insert into files (id, name, path) values (6, 'GRAND', 'img/logos/grand.png');
+insert into files (id, name, path) values (7, 'MEDIUM', 'img/logos/medium.png');
+insert into files (id, name, path) values (8, 'SMALL', 'img/logos/small.png');
--- db/scripts/011_dml_insert_halls.sql
+++ db/scripts/011_dml_insert_halls.sql
@@ -1,6 +1,6 @@
 insert into halls (id, name,   row_count,  place_count, description)
 values (1, 'GRAND', 10, 20, 'Huge Hall');
 insert into halls (id, name,   row_count,  place_count ,description)
-values (2, 'Medium', 8, 10, 'Medium Hall');
+values (2, 'MEDIUM', 8, 10, 'Medium Hall');
 insert into halls (id, name,   row_count,  place_count ,description)
 values (3, 'SMALL', 4, 8, 'For small movies');
